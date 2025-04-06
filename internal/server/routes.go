d3143d6f4dc32545c2a35a0d06c7901e83f851a6
Jack Kitto <jack.kitto@gmail.com>
2025-04-06 10:57:05 +0900

    feat: setup oauth

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"context"
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"log"
 	"net/http"
@@ -14,6 +16,9 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/adaptor"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/filesystem"
+	"github.com/gofiber/session/v2"
+
+	"github.com/shareed2k/goth_fiber"
 )
 
 func (s *FiberServer) RegisterFiberRoutes() {
@@ -38,6 +43,9 @@ func (s *FiberServer) RegisterFiberRoutes() {
 		PathPrefix: "assets",
 		Browse:     false,
 	}))
+	// OAuth routes
+	s.Get("/auth/:provider", s.authHandler)
+	s.Get("/auth/:provider/callback", s.authCallbackHandler)
 }
 
 func (s *FiberServer) healthHandler(c *fiber.Ctx) error {
@@ -72,3 +80,55 @@ func (s *FiberServer) websocketHandler(con *websocket.Conn) {
 		}
 	}
 }
+
+func (s *FiberServer) authHandler(c *fiber.Ctx) error {
+	err := goth_fiber.BeginAuthHandler(c)
+	if err != nil {
+		log.Println(err)
+	}
+	return nil
+}
+
+func (s *FiberServer) authCallbackHandler(c *fiber.Ctx) error {
+	user, err := goth_fiber.CompleteUserAuth(c)
+	if err != nil {
+		return c.Status(http.StatusUnauthorized).SendString(fmt.Sprintf("Authentication failed: %v", err))
+	}
+
+	sess := store.Get(c)
+	sess.Set("user", map[string]string{
+		"Name":     user.Name,
+		"Email":    user.Email,
+		"Provider": user.Provider,
+	})
+	save_err := sess.Save()
+	if save_err != nil {
+		log.Println(save_err)
+	}
+
+	return c.Redirect("/dashboard")
+}
+
+func (s *FiberServer) protectedMiddleware() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		sess := store.Get(c)
+
+		if sess.Get("user") == nil {
+			return c.Redirect("/")
+		}
+
+		return c.Next()
+	}
+}
+
+func (s *FiberServer) dashboardHandler(c *fiber.Ctx) error {
+	sess := store.Get(c)
+
+	user := sess.Get("user")
+	if user == nil {
+		return c.Status(http.StatusUnauthorized).SendString("Unauthorized")
+	}
+
+	return adaptor.HTTPHandler(templ.Handler(web.DashboardPage(user.(map[string]string))))(c)
+}
+
